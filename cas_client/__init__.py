f49f766682ee7fd9d69b5b197ab2cab0867bd687
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-04-11 15:10:30 -0700

    Improving setup.py, version info, readme.

--- cas_client/__init__.py
+++ cas_client/__init__.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
 import six
 
-
 if six.PY3:
     from .cas_client import *
+    from ._version import __version__, __version_info__
 else:
     from cas_client import *
+    from _version import __version__, __version_info__
--- setup.py
+++ setup.py
@@ -2,18 +2,50 @@
 import setuptools
 
 
-install_requires = [
-    'PyCrypto',
-    'requests',
-    'six',
-    'tox',
-    ]
+with open('README.rst', 'r') as file_pointer:
+    long_description = file_pointer.read()
+
+with open('cas_client/_version.py', 'r') as file_pointer:
+    exec(file_pointer.read())
 
 
-if __name__ == '__main__':
-    setuptools.setup(
-        include_package_data=True,
-        install_requires=install_requires,
-        name='cas_client',
-        packages=['cas_client'],
-        )
+setuptools.setup(
+    author='Josiah Wolf Oberholtzer',
+    author_email='joberholtzer@discogsinc.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        ],
+    description='A Python CAS client',
+    include_package_data=True,
+    install_requires=[
+        'PyCrypto',
+        'requests',
+        'six',
+        'tox',
+        ],
+    keywords=[
+        'auth',
+        'authentication',
+        'cas',
+        'cas2',
+        'cas3',
+        'client',
+        'single sign-on',
+        'sso',
+        ],
+    license='MIT',
+    long_description=long_description,
+    name='cas_client',
+    packages=['cas_client'],
+    platforms='any',
+    url='https://github.com/discogs/python-cas-client',
+    version=__version__,
+    )
